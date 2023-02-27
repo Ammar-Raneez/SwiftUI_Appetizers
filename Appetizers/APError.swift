9f0f78f4303dee26ed557df7456c22848dca7263
Ammar <ammarraneez@gmail.com>
2023-02-27 14:38:07 +0530

    Create network manager

---
+++ Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Ammar on 2023-02-27.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
---
+++ Appetizers/NetworkManager.swift
@@ -0,0 +1,55 @@
+//
+//  NetworkManager.swift
+//  Appetizers
+//
+//  Created by Ammar on 2023-02-27.
+//
+
+import Foundation
+
+final class NetworkManager {
+    // Create singleton network manager -> Ensure only a single object is created
+    static let shared = NetworkManager()
+    
+    static let baseURL = "https://seanallen-course-backend.herokuapp.com"
+    private let appetizerURL = "\(baseURL)/swiftui-fundamentals/appetizers"
+    
+    private init() {}
+    
+    // Result has a success case and a failure case that return an array of Appetizer or an Error respectively
+    func getAppetizers(completed: @escaping (Result<[Appetizer], APError>) -> Void) {
+        guard let url = URL(string: appetizerURL) else {
+            completed(.failure(.invalidURL))
+            return
+        }
+        
+        // Create url task and handle each of the 3 values
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            guard let _ = error else {
+                completed(.failure(.unableToComplete))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completed(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let decodedResponse = try decoder.decode(AppetizerResponse.self, from: data)
+                completed(.success(decodedResponse.request))
+            } catch {
+                completed(.failure(.invalidData))
+            }
+        }
+        
+        // Fire off network call
+        task.resume()
+    }
+}
