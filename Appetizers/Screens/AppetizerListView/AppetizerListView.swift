5e434a686b1eae0bcebca5c3eabbf9b09a5c31bf
Ammar <ammarraneez@gmail.com>
2023-02-28 18:14:17 +0530

    Show sample appetizer on click

--- Appetizers/Screens/AppetizerListView/AppetizerListView.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerListView.swift
@@ -9,17 +9,29 @@ import SwiftUI
 
 struct AppetizerListView: View {
     @StateObject var viewModel = AppetizerListViewModel()
+    @State private var isShowingDetail = false
     
     var body: some View {
         ZStack {
             NavigationView {
                 List(viewModel.appetizers) { appetizer in
                     AppetizerListCell(appetizer: appetizer)
+                        .onTapGesture {
+                            isShowingDetail = true
+                        }
                 }
                 .navigationTitle("Appetizers")
             }
             .onAppear {
                 viewModel.getAppetizers()
+            }
+            .blur(radius: isShowingDetail ? 20 : 0)
+            
+            if isShowingDetail {
+                AppetizerDetailView(
+                    isShowingDetail: $isShowingDetail,
+                    appetizer: MockData.sampleAppetizer
+                )
             }
             
             if viewModel.isLoading {
